832b9d26ee4f448fde99da918ce82d0ef9e169d1
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-22 14:37:46 +0530

    Create employee.py

---
+++ models/employee.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+
+    email = Column(String, unique=True)
+
+    department = Column(String)
+
+    role = Column(String)
